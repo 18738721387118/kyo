20a0ac7aa9499200f003c7c795b142bbb4ffb060
Yaroslav <yajafromhell@gmail.com>
2025-11-06 20:56:20 +0300

    refactor(often-bought): change shadow

--- apps/web/src/components/features/home/often-bought-card.tsx
+++ apps/web/src/components/features/home/often-bought-card.tsx
@@ -4,7 +4,7 @@ import { Card } from '@/components/shared/card'
 
 export function OftenBoughtCard() {
   return (
-    <Card className='flex w-1/4 flex-row items-center gap-4 rounded-xl border-none px-4 py-4 shadow-[0_0_25px_0_rgba(0,0,0,0.07)]'>
+    <Card className='flex w-1/4 cursor-pointer flex-row items-center gap-4 rounded-xl border-none px-4 py-4 shadow-[0_0_25px_0_rgba(0,0,0,0.07)] transition-all duration-300 hover:shadow-[0_0_10px_0_rgba(0,0,0,0.05)]'>
       <div className='shrink-0'>
         <Image
           src='/often-bought.jpg'
